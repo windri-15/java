276ee487e140f51308cfb7c4a96679752c177c65
windri-15 <windrisapitri294@gmail.com>
2024-11-16 13:14:50 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+public static void main(String[] args) {
+
+    String firstNama ="windri sapitri";
+    String lastNama ="Aulia";
+
+    System.out.println(firstNama.length());
+    System.out.println(lastNama.length());
+}
+}
